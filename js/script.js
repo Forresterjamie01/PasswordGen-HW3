ebcabb1ac23ab4fb61dc36fbbe94cfec3df4b090
Forresterjamie01 <Forresterjamie01@yahoo.com>
2021-03-26 23:10:31 -0400

    updated js password variables.

--- js/script.js
+++ js/script.js
@@ -5,12 +5,33 @@ var confirmCharacter;
 var confirmUppercase;
 var confirmLowercase;
 
-// Write password to the #password input
+//Password Variables
 // List of Special Characters
 character =["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 // Number Characters
 number = [1,2,3,4,5,6,7,8,9];
+// Alphabet Characters
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//Space for uppercase
+space =[];
+//for different choices
+var choices;
+// change letters to uppercase
+var toUpper = function(x) {
+  return x.toUppercase();
+};
+// varibale for uppercasing
+alpha2 = alpha.map(toUpper);
 
+var get = document.querySelector("generate");
+
+get.addEventListener("click",function(){
+  ps = generatePassword();
+  document.getElementById("password").placeholder = ps;
+});
+
+
+// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
